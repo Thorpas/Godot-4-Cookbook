dad1b54d1d02500801b425719afbdb6b08a2ead2
Thorpas <thorpas@gmx.net>
2023-06-09 21:29:28 +0200

    Update Match 3 Game

--- Start.gd
+++ Start.gd
@@ -1,5 +1,7 @@
 extends Node2D
 class_name StartScreen
 
+func _on_match_3_button_up():
+	get_tree().change_scene_to_packed(load("res://match3/Match3Game.tscn"))
 func _on_quit_button_up():
 	get_tree().quit()
--- match3/Match3Game.gd
+++ match3/Match3Game.gd
@@ -6,5 +6,6 @@ class_name Match3Game
 ## Godot
 func _ready():
 	$Match3Grid.initialize_grid(5,5,[&"Blue",&"Gold",&"Red"])
+
 func _on_return_button_up():
 	get_tree().change_scene_to_packed(load("res://Start.tscn"))

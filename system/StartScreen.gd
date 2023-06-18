f9754630c829e74541718ab9e173ba66ea055d6b
Thorpas <thorpas@gmx.net>
2023-06-18 19:21:43 +0200

    Update Cookbook
    Added Godot website link and license link to Start Screen.

--- system/StartScreen.gd
+++ system/StartScreen.gd
@@ -19,6 +19,13 @@ func _on_screen_2D_button_up():
 ### GitHub
 func _on_git_hub_button_up():
 	OS.shell_open("https://github.com/Thorpas/TPS-Cookbook")
+### Godot
+func _on_godot_button_up():
+	OS.shell_open("https://godotengine.org")
+func _on_license_button_up():
+	OS.shell_open("https://godotengine.org/license/")
+#	$Interface/Popup/License.set_text(Engine.get_license_text())
+#	$Interface/Popup.popup_centered()https://godotengine.org/license/
 ### Quit
 func _on_quit_button_up():
 	await transition_into_color(Color.BLACK, 0.5)

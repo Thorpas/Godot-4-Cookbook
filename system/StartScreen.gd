ba5559371684818c85bcc9a6828765f8048fa808
Thorpas <thorpas@gmx.net>
2023-06-18 23:24:23 +0200

    Update Cookbook
    Renamed to Godot 4 Cookbook

--- system/StartScreen.gd
+++ system/StartScreen.gd
@@ -18,14 +18,12 @@ func _on_screen_2D_button_up():
 	get_tree().change_scene_to_packed(load("res://system/showcases/screen2D/Screen2DShowcaseScreen.tscn"))
 ### GitHub
 func _on_git_hub_button_up():
-	OS.shell_open("https://github.com/Thorpas/TPS-Cookbook")
+	OS.shell_open("https://github.com/Thorpas/Godot-4-Cookbook")
 ### Godot
 func _on_godot_button_up():
 	OS.shell_open("https://godotengine.org")
 func _on_license_button_up():
 	OS.shell_open("https://godotengine.org/license/")
-#	$Interface/Popup/License.set_text(Engine.get_license_text())
-#	$Interface/Popup.popup_centered()https://godotengine.org/license/
 ### Quit
 func _on_quit_button_up():
 	await transition_into_color(Color.BLACK, 0.5)
